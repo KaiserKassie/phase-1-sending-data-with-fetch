e21833d27b4a2c258b8d32cfda82d27a7d66fac5
Kassie Becker <kassaundralee@hotmail.com>
2022-04-16 11:06:41 -0500

    commit tree 4637e3e7281a57faa6ee0a3d62119290a265c815

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(resp => resp.json())
+//     .then(data => console.log(data))
+
+
+
+function submitData(user, username) {
+    const formData = {
+        name: user,
+        email: username,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
+
+submitData("Kassie", "kassie.becker@gmail.com")
